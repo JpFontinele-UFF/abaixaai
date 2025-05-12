47ab3343633d0b240e1f4e98c2e06fc6e51fd2d1
JpFontinele-UFF <jfontinele@id.uff.br>
2025-05-12 09:27:18 -0300

    feat: geolocalização e mapa inicial

--- lib/app/ui/pages/home_page.dart
+++ lib/app/ui/pages/home_page.dart
@@ -1,40 +1,86 @@
 import 'package:abaixaai/app/controller/home_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:location/location.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  final HomeController _homeController = Get.put(HomeController());
+  final Location _location = Location();
+  LatLng? _currentLocation;
 
-   final HomeController _homeController = Get.put(HomeController());
+  @override
+  void initState() {
+    super.initState();
+    _getUserLocation();
+  }
+
+  Future<void> _getUserLocation() async {
+    final hasPermission = await _location.requestPermission();
+    if (hasPermission == PermissionStatus.granted) {
+      final locationData = await _location.getLocation();
+      setState(() {
+        _currentLocation = LatLng(locationData.latitude!, locationData.longitude!);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home"),
-        backgroundColor: Colors.blue,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage(_homeController.user.urlImage ?? "https://via.placeholder.com/150"),
+        centerTitle: true,
+        title: const Text("Abaixa Aí"),
+        backgroundColor: Colors.black,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.black, Colors.blue],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
             ),
-            const SizedBox(height: 20),
-            Text(
-              "Bem-vindo(a),",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            Text(
-              _homeController.user.name ?? "Usuário",
-              style: TextStyle(fontSize: 20, color: Colors.grey[700]),
-            ),
-          ],
+          ),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.menu, color: Colors.white),
+          onPressed: () {
+            // Ação do menu hambúrguer
+          },
         ),
       ),
+      body: _currentLocation == null
+          ? const Center(child: CircularProgressIndicator())
+          : FlutterMap(
+              options: MapOptions(
+                initialCenter: _currentLocation ?? LatLng(0.0, 0.0),
+                initialZoom: 15
+                
+              ),
+              children: [
+                TileLayer(
+                  urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                  subdomains: ['a', 'b', 'c'],
+                ),
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      point: _currentLocation!,
+                      child: const Icon(
+                        Icons.location_pin,
+                        color: Colors.red,
+                        size: 40,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
     );
   }
 }
