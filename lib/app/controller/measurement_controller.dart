619f56c884593e9c01be557960658d339bd06604
JpFontinele-UFF <jfontinele@id.uff.br>
2025-06-09 13:56:56 -0300

    update: email vira createdat

--- lib/app/controller/measurement_controller.dart
+++ lib/app/controller/measurement_controller.dart
@@ -27,18 +27,18 @@ class MeasurementController extends GetxController {
   Future<void> _requestPermission() async =>
       await Permission.microphone.request();
 
-void _onData(NoiseReading noiseReading) {
-  if (noiseReading.meanDecibel.isFinite) {
-    currentDb.value = noiseReading.meanDecibel;
-    dbValues.add(noiseReading.meanDecibel);
-    minDb.value = dbValues.reduce((a, b) => a < b ? a : b);
-    maxDb.value = dbValues.reduce((a, b) => a > b ? a : b);
-    avgDb.value = dbValues.reduce((a, b) => a + b) / dbValues.length;
-  } else {
-    currentDb.value = 0.0; // Valor padrão para casos inválidos
+  void _onData(NoiseReading noiseReading) {
+    if (noiseReading.meanDecibel.isFinite) {
+      currentDb.value = noiseReading.meanDecibel;
+      dbValues.add(noiseReading.meanDecibel);
+      minDb.value = dbValues.reduce((a, b) => a < b ? a : b);
+      maxDb.value = dbValues.reduce((a, b) => a > b ? a : b);
+      avgDb.value = dbValues.reduce((a, b) => a + b) / dbValues.length;
+    } else {
+      currentDb.value = 0.0; // Valor padrão para casos inválidos
+    }
+    update();
   }
-  update();
-}
 
   void _onError(Object error) {
     stopRecording();
@@ -71,131 +71,126 @@ void _onData(NoiseReading noiseReading) {
     return querySnapshot.docs.map((doc) => doc.data()).toList();
   }
 
-Future<void> saveMeasurement({
-  required double latitude,
-  required double longitude,
-  required String userEmail, // Adicionado o email do usuário
-}) async {
-  final denunciasCollection = FirebaseFirestore.instance.collection(
-    'denuncias',
-  );
+  Future<void> saveMeasurement({
+    required double latitude,
+    required double longitude,
+    required String userEmail, // Adicionado o email do usuário
+  }) async {
+    final denunciasCollection = FirebaseFirestore.instance.collection(
+      'denuncias',
+    );
 
-  final querySnapshot = await denunciasCollection.get();
-  bool isWithinRange = false;
-  String? parentDocId;
-  int low = 0;
-  int medium = 0;
-  int high = 0;
+    final querySnapshot = await denunciasCollection.get();
+    bool isWithinRange = false;
+    String? parentDocId;
+    int low = 0;
+    int medium = 0;
+    int high = 0;
 
-  if (avgDb.value < 40) {
-    low = 1;
-  } else if (avgDb.value < 80) {
-    medium = 1;
-  } else {
-    high = 1;
-  }
+    if (avgDb.value < 40) {
+      low = 1;
+    } else if (avgDb.value < 80) {
+      medium = 1;
+    } else {
+      high = 1;
+    }
 
-  for (var doc in querySnapshot.docs) {
-    final data = doc.data();
-    final existingLatitude = data['latitude'] as double?;
-    final existingLongitude = data['longitude'] as double?;
+    for (var doc in querySnapshot.docs) {
+      final data = doc.data();
+      final existingLatitude = data['latitude'] as double?;
+      final existingLongitude = data['longitude'] as double?;
 
-    if (existingLatitude != null && existingLongitude != null) {
-      final distance = _calculateDistance(
-        latitude,
-        longitude,
-        existingLatitude,
-        existingLongitude,
-      );
+      if (existingLatitude != null && existingLongitude != null) {
+        final distance = _calculateDistance(
+          latitude,
+          longitude,
+          existingLatitude,
+          existingLongitude,
+        );
 
-      if (distance <= 50) {
-        isWithinRange = true;
-        parentDocId = doc.id;
-        break;
+        if (distance <= 50) {
+          isWithinRange = true;
+          parentDocId = doc.id;
+          break;
+        }
       }
     }
+
+    if (isWithinRange && parentDocId != null) {
+      final docSnapshot = await denunciasCollection.doc(parentDocId).get();
+
+      // Obtém os valores de min, max e avg
+      final currentData = docSnapshot.data() ?? {};
+      double currentMin = currentData['min'] ?? 0;
+      double currentMax = currentData['max'] ?? 0;
+      double currentAvg = currentData['avg'] ?? 0;
+
+      final currentAmount = docSnapshot.data()?['amount'] ?? {};
+      int currentLow = currentAmount['low'] ?? 0;
+      int currentMedium = currentAmount['medium'] ?? 0;
+      int currentHigh = currentAmount['high'] ?? 0;
+
+      int total =
+          currentLow + low + currentMedium + medium + currentHigh + high;
+
+      double newMin = (currentMin + minDb.value) / total;
+      double newMax = (currentMax + maxDb.value) / total;
+      double newAvg = (currentAvg + avgDb.value) / total;
+
+      // Guarda a nova denúncia na sub_denúncia
+      await denunciasCollection.doc(parentDocId).collection('sub_denuncias').add({
+        'min': minDb.value,
+        'max': maxDb.value,
+        'avg': avgDb.value,
+        'latitude': latitude,
+        'longitude': longitude,
+        'timestamp':
+            FieldValue.serverTimestamp(), // Corrigido para salvar como Timestamp
+        'createdAt': userEmail, // Adicionado o email do usuário
+      });
+
+      // Atualiza a denúncia principal
+      await denunciasCollection.doc(parentDocId).update({
+        'min': newMin,
+        'max': newMax,
+        'avg': newAvg,
+        'timestamp':
+            FieldValue.serverTimestamp(), // Corrigido para salvar como Timestamp
+        'amount': {
+          'low': currentLow + low,
+          'medium': currentMedium + medium,
+          'high': currentHigh + high,
+        },
+      });
+    } else {
+      // Se não houver denúncia dentro do raio, cria uma nova
+      final documentRef = await denunciasCollection.add({
+        'min': minDb.value,
+        'max': maxDb.value,
+        'avg': avgDb.value,
+        'latitude': latitude,
+        'longitude': longitude,
+        'timestamp':
+            FieldValue.serverTimestamp(), // Corrigido para salvar como Timestamp
+        'amount': {'low': low, 'medium': medium, 'high': high},
+        'createdAt': userEmail, // Adicionado o email do usuário
+      });
+
+      final documentId = documentRef.id;
+
+      // Adiciona a sub_denúncia na nova denúncia
+      await denunciasCollection.doc(documentId).collection('sub_denuncias').add({
+        'min': minDb.value,
+        'max': maxDb.value,
+        'avg': avgDb.value,
+        'latitude': latitude,
+        'longitude': longitude,
+        'timestamp':
+            FieldValue.serverTimestamp(), // Corrigido para salvar como Timestamp
+        'createdAt': userEmail, // Adicionado o email do usuário
+      });
+    }
   }
-
-  if (isWithinRange && parentDocId != null) {
-    final docSnapshot = await denunciasCollection.doc(parentDocId).get();
-
-    // Obtém os valores de min, max e avg
-    final currentData = docSnapshot.data() ?? {};
-    double currentMin = currentData['min'] ?? 0;
-    double currentMax = currentData['max'] ?? 0;
-    double currentAvg = currentData['avg'] ?? 0;
-
-    final currentAmount = docSnapshot.data()?['amount'] ?? {};
-    int currentLow = currentAmount['low'] ?? 0;
-    int currentMedium = currentAmount['medium'] ?? 0;
-    int currentHigh = currentAmount['high'] ?? 0;
-
-    int total =
-        currentLow + low + currentMedium + medium + currentHigh + high;
-
-    double newMin = (currentMin + minDb.value) / total;
-    double newMax = (currentMax + maxDb.value) / total;
-    double newAvg = (currentAvg + avgDb.value) / total;
-
-    // Guarda a nova denúncia na sub_denúncia
-    await denunciasCollection
-        .doc(parentDocId)
-        .collection('sub_denuncias')
-        .add({
-          'min': minDb.value,
-          'max': maxDb.value,
-          'avg': avgDb.value,
-          'latitude': latitude,
-          'longitude': longitude,
-          'timestamp': FieldValue.serverTimestamp(), // Corrigido para salvar como Timestamp
-          'createdAt': '',
-          'email': userEmail, // Adicionado o email do usuário
-        });
-
-    // Atualiza a denúncia principal
-    await denunciasCollection.doc(parentDocId).update({
-      'min': newMin,
-      'max': newMax,
-      'avg': newAvg,
-      'timestamp': FieldValue.serverTimestamp(), // Corrigido para salvar como Timestamp
-      'amount': {
-        'low': currentLow + low,
-        'medium': currentMedium + medium,
-        'high': currentHigh + high,
-      },
-    });
-  } else {
-    // Se não houver denúncia dentro do raio, cria uma nova
-    final documentRef = await denunciasCollection.add({
-      'min': minDb.value,
-      'max': maxDb.value,
-      'avg': avgDb.value,
-      'latitude': latitude,
-      'longitude': longitude,
-      'timestamp': FieldValue.serverTimestamp(), // Corrigido para salvar como Timestamp
-      'createdAt': '',
-      'amount': {'low': low, 'medium': medium, 'high': high},
-      'email': userEmail, // Adicionado o email do usuário
-    });
-
-    final documentId = documentRef.id;
-
-    // Adiciona a sub_denúncia na nova denúncia
-    await denunciasCollection
-        .doc(documentId)
-        .collection('sub_denuncias')
-        .add({
-          'min': minDb.value,
-          'max': maxDb.value,
-          'avg': avgDb.value,
-          'latitude': latitude,
-          'longitude': longitude,
-          'timestamp': FieldValue.serverTimestamp(), // Corrigido para salvar como Timestamp
-          'createdAt': '',
-          'email': userEmail, // Adicionado o email do usuário
-        });
-  }
-}
 
   double _calculateDistance(
     double lat1,
